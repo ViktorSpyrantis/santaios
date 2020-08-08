dd1e7e49ebf32d837704c13b1a8f34f4cfe0d5d6
Viktor <victorneasanta@yahoo.gr>
2020-08-08 23:36:27 +0300

    added container div for buttons, aligniment seems correct

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -8,14 +8,19 @@ import { Router, NavigationExtras } from '@angular/router';
 })
 export class Dashboard {
 
-  private categoryList = Object.values(productCategories);
+  categoryList = [];
   private productsList = productCategories;
-  private bgImage: string = "assets/img/bg_image.png"
+  bgImage: string = "assets/img/bg.png"
+  loadingComplete = false;
 
   constructor(
     private router: Router
   ) {
 
+  }
+
+  ngOnInit() {
+    this.initCategoryList();
   }
 
   openCategory(categoryName: string) {
@@ -29,15 +34,62 @@ export class Dashboard {
     this.router.navigate(['/category'], navigationExtras)
   }
 
+  private initCategoryList() {
+    this.categoryList = [
+      {
+        id: 'BEEF',
+        greek: 'ΜΟΣΧΑΡΙΣΙΟ',
+        icon: 'cow.svg',
+        icon_by_height: true
+      },
+      {
+        id: 'PORK',
+        greek: 'ΧΟΙΡΙΝΟ',
+        icon: 'pig.svg',
+        icon_by_height: true
+      },
+      {
+        id: 'CHICKEN',
+        greek: 'ΚΟΤΟΠΟΥΛΟ',
+        icon: 'chicken.svg',
+        icon_by_height: false
+      },
+      {
+        id: 'LAMB',
+        greek: 'ΑΜΝΟΕΡΙΦΙΑ',
+        icon: 'lamb.svg',
+        icon_by_height: true
+      },
+      {
+        id: 'PREPARATIONS',
+        greek: 'ΠΑΡΑΣΚΕΥΑΣΜΑΤΑ',
+        icon: 'kebab.svg',
+        icon_by_height: false
+      },
+      {
+        id: 'DRY_AGED',
+        greek: 'DRY AGED',
+        icon: 'steak.svg',
+        icon_by_height: true
+      }
+    ];
+
+    this.categoryList.forEach(cat => {
+      cat.icon = 'assets/icon/' + cat.icon;
+    })
+    this.loadingComplete = true;
+  }
 }
 
+
+
 export enum productCategories {
-  BEEF = 'ΜΟΣΧΑΡΙΣΙΟ',
-  PORK = 'ΧΟΙΡΙΝΟ',
-  CHICKEN = 'ΚΟΤΟΠΟΥΛΟ',
-  LAMB = 'ΑΜΝΟΕΡΙΦΙΑ',
-  PREPARATIONS = 'ΠΑΡΑΣΚΕΥΑΣΜΑΤΑ',
-  DRY_AGED = 'DRY AGED'
+  BEEF = 'BEEF',
+  PORK = 'PORK',
+  CHICKEN = 'CHICKEN',
+  LAMB = 'LAMB',
+  PREPARATIONS = 'PREPARATIONS',
+  DRY_AGED = 'DRY_AGED'
 }
 
 
